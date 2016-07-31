ad34683888431622c81d7bba0d91b9ad419a2b89
chongtw0990@gmail.com <chongtw0990@gmail.com>
2016-07-31 22:39:13 +0800

    User sign up, Flash added. Input validation Added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ApplicationRecord
+  before_save { self.email.downcase! }
+
+
+  # VALIDATION
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates(  :name,
+              presence: true,
+              length: { maximum: 50, message: 'Input name is too long!' }
+  )
+
+  validates(  :email,
+              presence: true,
+              length: { maximum: 75 },
+              format: { with: VALID_EMAIL_REGEX },
+              uniqueness: { case_sensitive: false }
+  )
+validates( :password_digest, presence: true, length: { minimum: 3 })
+
+# FOR HASHING PASSWORD
+  has_secure_password
+
+
+# Relationship model
   has_many :reviews
+
 end
