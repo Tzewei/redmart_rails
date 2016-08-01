6cba397e91e852074ba82ea3434ba0b441fb6ecb
chongtw0990@gmail.com <chongtw0990@gmail.com>
2016-08-01 11:10:13 +0800

    Password_digest working

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :users
   root 'static_pages#home'
 
+  resources :products
+  resources :users
+
+  # static pages routes, not connected to models
   get '/help',    to: 'static_pages#help', as: 'helf'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/signup',  to: 'users#new'
-  
 
+  # dynamic routes
+  get '/signup',  to: 'users#new'
+
+
+  # login routes
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
